f1ec36ed7c4f5d21c66d5195b7d787a04bacb55e
Ruslan.S <ruslan-shahaev@mail.ru>
2018-04-25 22:44:19 +0300

    Second part

---
+++ homeworks/profile/profile_decorator.py
@@ -0,0 +1,54 @@
+import functools
+import time
+
+
+def profile(input):
+    if isinstance(input, type):
+        for attr_name in dir(input):
+            if attr_name[:2] != '__' or attr_name == '__init__':
+                attr = getattr(input, attr_name)
+                attr.status = True
+                attr.class_name = input.__name__
+                setattr(input, attr_name, profile(attr))
+        return input
+
+
+    @functools.wraps(input)
+    def func_with_timer(*args, **kwargs):
+        if getattr(input, 'status', None):
+            print(input.class_name + '.' + input.__name__ + ' started')
+        else:
+            print(input.__name__ + ' started')
+        start = time.time()
+        result = input(*args, **kwargs)
+        stop = time.time()
+        global add_class_name_to_output_str
+        if getattr(input, 'status', None):
+            print(input.class_name + '.' + input.__name__ + ' finished in {0}s'.format(round((stop - start), 3)))
+        else:
+            print(input.__name__ + ' finished in {0}s'.format(round((stop - start), 3)))
+        return result
+    return func_with_timer
+
+
+@profile
+def qoo(*args, **kwargs):
+    time.sleep(0.001)
+
+
+@profile
+class Boo:
+    def __init__(self):
+        time.sleep(0.001)
+
+    def foo(self):
+        time.sleep(0.001)
+
+    def koo(self):
+        time.sleep(0.001)
+
+
+b = Boo()
+b.foo()
+qoo(2, 3)
+b.koo()
