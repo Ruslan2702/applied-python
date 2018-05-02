40bf117c7b5b15fbe60e4ba82059526644a269ba
Ruslan.S <ruslan-shahaev@mail.ru>
2018-05-02 21:57:47 +0300

    Dirdict corrected

--- homeworks/dir_dict/dirdict.py
+++ homeworks/dir_dict/dirdict.py
@@ -6,26 +6,24 @@ import subprocess
 class DirDict(collections.MutableMapping):
     def __init__(self, dir_path):
         self._dir_path = dir_path
-        if dir_path[len(dir_path) - 1] != '/':
-            self._dir_path += '/'
 
     def __len__(self):
         return len(os.listdir(path=self._dir_path))
 
     def __setitem__(self, key, value):
-        with open(self._dir_path + str(key), 'w') as f:
+        with open(os.path.join(self._dir_path, str(key)), 'w') as f:
             f.write(str(value))
 
     def __getitem__(self, key):
         if str(key) not in os.listdir(path=self._dir_path):
             raise KeyError(str(key))
-        with open(self._dir_path + str(key), 'r') as f:
+        with open(os.path.join(self._dir_path, str(key)), 'r') as f:
             return f.read()
 
     def __delitem__(self, key):
         if str(key) not in os.listdir(path=self._dir_path):
             raise KeyError(str(key))
-        subprocess.call(['rm', self._dir_path + str(key)])
+        subprocess.call(['rm', os.path.join(self._dir_path, str(key))])
 
     def __iter__(self):
         return iter(os.listdir(path=self._dir_path))
