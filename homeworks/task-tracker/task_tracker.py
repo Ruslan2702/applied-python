e559baa1b3c59d8de65f8d75069ddc9d64d79a1a
Ruslan.S <ruslan-shahaev@mail.ru>
2018-05-14 19:19:44 +0300

    Recursion

--- homeworks/task-tracker/task_tracker.py
+++ homeworks/task-tracker/task_tracker.py
@@ -34,17 +34,35 @@ class TackTracker:
 
     def mark_task_completed(self, t_id):
         sql_mark_task_completed = f"""
-            UPDATE tasks SET task_type=2 WHERE id={t_id} OR parent_id={t_id}
+            UPDATE tasks SET task_type=2 WHERE id={t_id}
         """
         self._cursor.execute(sql_mark_task_completed)
+
+        sql_get_sub_task = f"""
+            SELECT id FROM tasks WHERE parent_id={t_id}
+        """
+        self._cursor.execute(sql_get_sub_task)
+        sub_task_id = self._cursor.fetchall()
+        if sub_task_id:
+            self.mark_task_completed(sub_task_id[0]['id'])
+
         self._connection.commit()
 
     def assign_task_to_user(self, t_id, u_name):
         sql_add_user_id_in_tasks = f"""
             UPDATE tasks SET user_token=(SELECT id FROM users WHERE login='{u_name}') 
-            WHERE id={t_id} OR parent_id={t_id}
+            WHERE id={t_id}
         """
         self._cursor.execute(sql_add_user_id_in_tasks)
+
+        sql_get_sub_task = f"""
+                    SELECT id FROM tasks WHERE parent_id={t_id}
+                """
+        self._cursor.execute(sql_get_sub_task)
+        sub_task_id = self._cursor.fetchall()
+        if sub_task_id:
+            self.assign_task_to_user(sub_task_id[0]['id'], u_name)
+
         self._connection.commit()
 
     def get_task_status(self, t_id):
@@ -78,6 +96,8 @@ class TackTracker:
         self._cursor.execute(sql_to_delete)
         sql_to_delete = "DELETE FROM users"
         self._cursor.execute(sql_to_delete)
+        sql_to_delete = "ALTER TABLE tasks AUTO_INCREMENT=0"
+        self._cursor.execute(sql_to_delete)
         self._connection.commit()
 
     def create_user(self, u_name):
@@ -95,11 +115,14 @@ if __name__ == '__main__':
     t_tracker = TackTracker('testDB', 'localhost', 'test_user', 'password')
     t_tracker._clear_the_base()
     t_tracker.add_task()
-    t_tracker.mark_task_completed(9)
+    t_tracker.add_task()
+    t_tracker.add_task()
+    t_tracker.add_task()
+    t_tracker.add_sub_task(1, 2)
+    t_tracker.add_sub_task(2, 3)
+    t_tracker.add_sub_task(3, 4)
+    t_tracker.mark_task_completed(1)
     t_tracker.create_user('Lion')
-    t_tracker.assign_task_to_user(14, 'Lion')
-    t_tracker.add_task()
-    t_tracker.add_task()
+    t_tracker.assign_task_to_user(1, 'Lion')
     print(t_tracker.get_tasks_list())
-    t_tracker.add_sub_task(25, 26)
-    t_tracker.mark_task_completed(25)
+
